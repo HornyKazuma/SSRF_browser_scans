9953c0f281dd43b68adb2c16c3476ad90aa3e1d4
HornyKazuma <me@hornykazuma.ru>
2024-05-16 16:52:23 +0300

    Update ip_scan.js
    v5

--- ip_scan.js
+++ ip_scan.js
@@ -3,10 +3,12 @@ let responsiveIps = [];
 for (let i = 0; i <= 255; i++) {
     for (let j = 0; j <= 255; j++) {
         let ip = ipBase + i + '.' + j + '.1';
-        fetch(`https://${ip}`)
+        fetch(`https://${ip}`, { mode: 'no-cors' })
             .then(response => {
-                responsiveIps.push(ip);
-                console.log(`Response from: ${ip}`);
+                if (response.ok) {
+                    responsiveIps.push(ip);
+                    console.log(`Response from: ${ip}`);
+                }
             })
             .catch(error => {
             });
